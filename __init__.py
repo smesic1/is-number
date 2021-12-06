a1a725465401bc7e75d7797baddd65452608b8cb
Selma Mesic <selma.sm.mesic@gmail.com>
2021-12-06 21:58:25 +0000

    Apply Black

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from .is_number import is_number 
+from .is_number import is_number
 from ._version import get_version
-__version__ = _version.get_versions()['version']
-del get_versions
 
+__version__ = _version.get_versions()["version"]
+del get_versions
